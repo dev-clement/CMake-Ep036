041fefc7aea578c6d168806bd0591207fb9173ac
Clement FRANCOIS <clement.francois.developpeur@outlook.com>
2025-07-18 17:14:18 +0200

    Initial commit

---
+++ app/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "my_lib.hpp"
+#include "config.hpp"
+
+int main() {
+    std::cout   << project_name << std::endl
+                << project_version_major << "." << project_version_minor << "." << project_version_patch << std::endl;
+    print_hello_world();
+    return EXIT_SUCCESS;
+}
